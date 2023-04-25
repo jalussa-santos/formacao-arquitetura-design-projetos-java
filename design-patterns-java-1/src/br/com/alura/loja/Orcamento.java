f7884a10f9cd9c728420b5beb57c406b2d0ee137
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2023-04-25 08:57:38 -0300

    Ajuste código para state

--- design-patterns-java-1/src/br/com/alura/loja/Orcamento.java
+++ design-patterns-java-1/src/br/com/alura/loja/Orcamento.java
@@ -7,6 +7,8 @@ public class Orcamento {
 	private BigDecimal valor;
 	private int quantidadeItens;
 
+	private String situacao;
+
 	public Orcamento(BigDecimal valor, int quantidadeItens) {
 		this.valor = valor;
 		this.quantidadeItens = quantidadeItens;
@@ -18,6 +20,21 @@ public class Orcamento {
 
 	public int getQuantidadeItens() {
 		return quantidadeItens;
+	}
+
+	public void aplicarDescontoExtra() {
+		BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+
+		if (situacao.equals("EM_ANALISE")) {
+			valorDoDescontoExtra = new BigDecimal("0.05");
+		} else if (situacao.equals("APROVADO")) {
+			valorDoDescontoExtra = new BigDecimal("0.02");
+		}
+		this.valor = this.valor.subtract(valorDoDescontoExtra);
+	}
+
+	public void aprovar() {
+		this.situacao = "APROVADO";
 	}
 
 }
