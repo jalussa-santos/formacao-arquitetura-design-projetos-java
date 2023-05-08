1004321566c7a20dde7a63f8dcc97c538f9c1dcb
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2023-05-08 08:48:08 -0300

    preparação para aplicação do design pattern command

---
+++ design-patterns-java-1/src/br/com/alura/loja/TestesPedidos.java
@@ -0,0 +1,22 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.Pedido;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+class TestesPedidos {
+
+  public static void main(String[] args) {
+    Orcamento orcamento = new Orcamento(new BigDecimal("600"),4);
+    String cliente = "Ana da Silva";
+    LocalDateTime data = LocalDateTime.now();
+
+    Pedido pedido = new Pedido(cliente, data, orcamento);
+
+    System.out.println("Salvar pedido no banco de dados");
+    System.out.println("Enviar e-mail com dado do novo pedido");
+
+  }
+
+}
