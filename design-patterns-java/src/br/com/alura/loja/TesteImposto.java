cdafbf2403c40735badeb8cd61b339ea9a7ece5b
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2023-06-24 17:02:28 -0300

    ajuste main

--- design-patterns-java/src/br/com/alura/loja/TesteImposto.java
+++ design-patterns-java/src/br/com/alura/loja/TesteImposto.java
@@ -6,7 +6,7 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class TesteImpostos {
+public class TesteImposto {
 
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"),1);
