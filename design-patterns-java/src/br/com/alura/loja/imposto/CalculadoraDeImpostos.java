d4617064ceecd8077a715aca5bb7d6fb38edb2dd
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2023-06-24 18:05:44 -0300

    ajuste style

--- design-patterns-java/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
+++ design-patterns-java/src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
@@ -1,14 +1,11 @@
 package br.com.alura.loja.imposto;
 
-import java.math.BigDecimal;
-
 import br.com.alura.loja.orcamento.Orcamento;
+import java.math.BigDecimal;
 
 public class CalculadoraDeImpostos {
 
-	public BigDecimal calcular(Orcamento orcamento, Imposto imposto) {
-		return imposto.calcular(orcamento);
-
-	}
-
+  public BigDecimal calcular(Orcamento orcamento, Imposto imposto) {
+    return imposto.calcular(orcamento);
+  }
 }
--- design-patterns-java/src/br/com/alura/loja/imposto/ICMS.java
+++ design-patterns-java/src/br/com/alura/loja/imposto/ICMS.java
@@ -7,6 +7,5 @@ public class ICMS implements Imposto {
 
   public BigDecimal calcular(Orcamento orcamento) {
     return orcamento.getValor().multiply(new BigDecimal("0.1"));
-
   }
 }
