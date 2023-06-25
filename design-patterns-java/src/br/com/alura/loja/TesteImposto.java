2550f14a4eeb3bfaf72e8de7a0565a64861e142a
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2023-06-25 18:56:03 -0300

    ajuste instancias de Orcamento

--- design-patterns-java/src/br/com/alura/loja/TesteImposto.java
+++ design-patterns-java/src/br/com/alura/loja/TesteImposto.java
@@ -3,13 +3,16 @@ package br.com.alura.loja;
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
 import java.math.BigDecimal;
 
 public class TesteImposto {
 
   public static void main(String[] args) {
-    Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
+    Orcamento orcamento = new Orcamento();
+    orcamento.adicionarItem(new ItemOrcamento(new BigDecimal("100")));
+
     CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
     System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS(new ICMS(null))));
     System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS(null)));
