ec4c45694ba056154c1027b3e73bb38fc05a4cbe
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2023-12-16 20:58:22 -0300

    ajuste classe bonus

--- testes-unitarios-tdd-java/src/main/br/com/alura/tdd/service/BonusService.java
+++ testes-unitarios-tdd-java/src/main/br/com/alura/tdd/service/BonusService.java
@@ -5,12 +5,13 @@ import br.com.alura.tdd.modelo.Funcionario;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class  BonusService {
+public class BonusService {
 
   public BigDecimal calcularBonus(Funcionario funcionario) {
     BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
     if (valor.compareTo(new BigDecimal("1000")) > 0) {
-      throw new IllegalArgumentException("Funcionário com salário maior do que R$10000 nao pode receber bonus!");
+      throw new IllegalArgumentException(
+          "Funcionário com salário maior do que R$10000 nao pode receber bonus!");
     }
     return valor.setScale(2, RoundingMode.HALF_UP);
   }
